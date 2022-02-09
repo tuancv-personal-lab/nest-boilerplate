0398436eb17b07087554793c37d58715d65d1266
tuancv-personal-lab <86569379+tuancv-personal-lab@users.noreply.github.com>
2022-02-09 22:21:28 +0700

    Initialize feat/grpc

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,106 @@
+import {
+    Body,
+    Controller,
+    Get,
+    HttpCode,
+    HttpStatus,
+    Post,
+    UploadedFile,
+    UseGuards,
+    UseInterceptors,
+} from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { FileInterceptor } from '@nestjs/platform-express';
+import {
+    ApiBearerAuth,
+    ApiImplicitFile,
+    ApiOkResponse,
+    ApiUseTags,
+} from '@nestjs/swagger';
+
+import { AuthUser } from '../../decorators/auth-user.decorator';
+import { AuthGuard } from '../../guards/auth.guard';
+import { AuthUserInterceptor } from '../../interceptors/auth-user-interceptor.service';
+import { IFile } from '../../interfaces/IFile';
+import { UserDto } from '../user/dto/UserDto';
+import { UserEntity } from '../user/user.entity';
+import { UserService } from '../user/user.service';
+import { AuthService } from './auth.service';
+import { LoginPayloadDto } from './dto/LoginPayloadDto';
+import { UserLoginDto } from './dto/UserLoginDto';
+import { UserRegisterDto } from './dto/UserRegisterDto';
+
+@Controller('auth')
+@ApiUseTags('auth')
+export class AuthController {
+    constructor(
+        public readonly userService: UserService,
+        public readonly authService: AuthService,
+    ) {}
+
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    @ApiOkResponse({
+        type: LoginPayloadDto,
+        description: 'User info with access token',
+    })
+    async userLogin(
+        @Body() userLoginDto: UserLoginDto,
+    ): Promise<LoginPayloadDto> {
+        const userEntity = await this.authService.validateUser(userLoginDto);
+
+        const token = await this.authService.createToken(userEntity);
+        return new LoginPayloadDto(userEntity.toDto(), token);
+    }
+
+    @GrpcMethod('AuthController', 'FindOne')
+    findOne(data: any, metadata: any): any {
+        console.log(data.id);
+        const items = [
+            { id: 1, name: 'John' },
+            { id: 2, name: 'Doe' },
+        ];
+        const a = items.find(({ id }) => id === data.id);
+
+        return a;
+    }
+
+    @GrpcMethod('AuthController', 'FindMany')
+    findMany(data: any, metadata: any): any {
+        console.log(data.id);
+        const items = [
+            { id: 1, name: 'John' },
+            { id: 2, name: 'Doe' },
+        ];
+        const a = items.find(({ id }) => id === data.id);
+
+        return a;
+    }
+
+    @Post('register')
+    @HttpCode(HttpStatus.OK)
+    @ApiOkResponse({ type: UserDto, description: 'Successfully Registered' })
+    @ApiImplicitFile({ name: 'avatar', required: true })
+    @UseInterceptors(FileInterceptor('avatar'))
+    async userRegister(
+        @Body() userRegisterDto: UserRegisterDto,
+        @UploadedFile() file: IFile,
+    ): Promise<UserDto> {
+        const createdUser = await this.userService.createUser(
+            userRegisterDto,
+            file,
+        );
+
+        return createdUser.toDto();
+    }
+
+    @Get('me')
+    @HttpCode(HttpStatus.OK)
+    @UseGuards(AuthGuard)
+    @UseInterceptors(AuthUserInterceptor)
+    @ApiBearerAuth()
+    @ApiOkResponse({ type: UserDto, description: 'current user info' })
+    getCurrentUser(@AuthUser() user: UserEntity) {
+        return user.toDto();
+    }
+}
